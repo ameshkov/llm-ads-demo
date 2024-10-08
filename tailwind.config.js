acb1c44b3ab46e93171149b7492637bcaee4eff3
Andrey Meshkov <am@adguard.com>
2024-10-08 23:08:19 +0300

    added empty demo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/index.html",
+    "./src/*.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js",
   ],
