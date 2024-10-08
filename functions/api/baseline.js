2ac29e8ef22825612920935f701cf5333d79d30a
Andrey Meshkov <am@adguard.com>
2024-10-08 23:33:20 +0300

    added cf func

--- functions/api/baseline.js
+++ functions/api/baseline.js
@@ -1,3 +1,3 @@
 export function onRequest(context) {
-    return new Response("This is a local environment!");
+    return new Response(`This is a local environment: ${context.env.API_TOKEN}!`);
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-/** @type {import('vite').UserConfig} */
-export default {
+import { defineConfig } from 'vite';
+
+import CloudflarePagesFunctions from 'vite-plugin-cloudflare-functions';
+
+export default defineConfig({
     root: 'src',
     build: {
         outDir: '../dist',
-    }
-}
+    },
+    plugins: [
+        CloudflarePagesFunctions({
+            root: 'functions',
+            outDir: '../dist',
+            dts: false,
+            wrangler: {
+                log: true,
+                compatibilityDate: '2024-10-08',
+                binding: {
+                    API_TOKEN: 'atata',
+                },
+            }
+        })
+    ]
+});
